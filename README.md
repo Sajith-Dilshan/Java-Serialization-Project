7cfedcde64d7c8cccfbfc14b4e6d0be98f7a627d
sajith <sajithd784@gmail.com>
2022-03-13 13:47:19 +0530

    initialize application

---
+++ README.md
@@ -0,0 +1,13 @@
+#Java Serialization Project
+<hr>
+
+
+### Version
+1.0.0
+<hr>
+
+###License
+Copyright © 2022 DEP8. All Rights Reserved.
+
+This project is licensed under [MIT](LICENSE.txt) License.
+
---
+++ src/main/java/me/sajithdilshan/dep8/Controller/MainFormController.java
@@ -0,0 +1,21 @@
+package me.sajithdilshan.dep8.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MainFormController {
+
+
+    public TextField txtId;
+    public TextField txtName;
+    public TextField txtAddress;
+    public TextField txtImage;
+    public TableView tblCustomers;
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBrowse_OnAction(ActionEvent actionEvent) {
+    }
+}
